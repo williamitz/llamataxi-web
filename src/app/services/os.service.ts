6ee934ebe5a600ca53716f227ec1f43ebf90b422
william calle <calle.usp@gmail.com>
2020-12-20 21:39:46 -0500

    avance vie 18 dic

--- src/app/services/os.service.ts
+++ src/app/services/os.service.ts
@@ -16,7 +16,7 @@ export class OsService {
   onSendPushUser( osId: string[], title: string, message: string ) {
 
     this.st.onLoadToken();
-    const body = { message, title, osId, data: { accepted: false, declined: false } };
+    const body = { message, title, osId, data: { accepted: false, declined: false }, template_id: '9d2ad516-a641-4d44-b000-15d6adbbebdc' };
     return this.http.post<IResponse>( URI_API + '/Push/Send', body, {headers: { Authorization: this.st.token }} );
   }
 }
